aae0b227e5516fdc66cd1f1d0dae4a6053bc0764
elik <elik@example.com>
2025-07-03 19:38:43 +0200

    fix_models

--- bibliogame/bibliogames/models.py
+++ bibliogame/bibliogames/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,13 +5,13 @@ class Game(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     release_date = models.DateField()
-    developer = models.ForeignKey('Developer', on_delete=models.SET_NULL)
+    developer = models.ForeignKey('Developer', on_delete=models.CASCADE)
     genres = models.ManyToManyField('Genre')
     platforms = models.ManyToManyField('Platforms')
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
 
     class Meta:
-        ordering = ['-release_data']
+        ordering = ['-release_date']
         db_table = 'games'
 
     def __str__(self):
@@ -31,6 +30,6 @@ class Genre(models.Model):
         ordering = ['name']
 
 
-class Platform(models.Model):
+class Platforms(models.Model):
     name = models.CharField(max_length=100)
 
