231033c396581448c3ca702d75396ae721010cad
elik <elik@example.com>
2025-07-10 19:21:07 +0200

    new 2 models and 2 views

--- bibliogame/bibliogames/views/views.py
+++ bibliogame/bibliogames/views/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
 from ..forms import GameCreateForm
-from bibliogames.models import Game
+from bibliogames.models import Game, Favorites, FavoriteGame
 
 
 def create_game(request):
@@ -26,4 +27,34 @@ def delete_game(request, game_id):
     else:
         return HttpResponseForbidden("you can't delete the game because you don't have rights")
 
+
+def add_favorite_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+
+    if not request.user.is_authenticated():
+        favorites = request.session.get(settings.FAVORITE_SESSION_ID, {})
+        request.session[settings.FAVORITE_SESSION_ID] = favorites
+    else:
+        favorites = request.user.favorites
+        favorite_game = FavoriteGame.objects.get_or_create(favorites=favorites, game=game)
+        favorite_game.save()
+    return redirect("")
+
+
+def delete_favorite_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+
+    if not request.user.is_authenticated():
+        favorites = request.session.get(settings.FAVORITE_SESSION_ID, {})
+        if game_id in favorites:
+            del favorites[game_id]
+            request.session[settings.FAVORITE_SESSION_ID] = favorites
+    else:
+        try:
+            favorites = request.user.favorites
+            game_del = FavoriteGame.objects.get(favorites=favorites, game=game)
+            game_del.delete()
+        except FavoriteGame.DoesNotExist:
+            favorites = None
+    return redirect("")
 
