5045152331b4cbc26534e25723e01ea83fd48688
Matviy4ik <matvejbajdik1@gmail.com>
2025-07-22 18:59:25 +0300

    urls

--- bibliogame/bibliogames/urls.py
+++ bibliogame/bibliogames/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from .views.game import index
 from .views.views import create_game, delete_game, add_favorite_game, delete_favorite_game
 from .views.review import add_review, delete_review
 
 app_name = "bibliogames"
 
 urlpatterns = [
+    path('', index, name='index'),
     path('game/add/', create_game, name='create_game'),
     path('game/delete/<int:game_id>/', delete_game, name='delete_game'),
     path('favorite_game/add/<int:game_id>/', add_favorite_game, name='add_favorite_game'),
